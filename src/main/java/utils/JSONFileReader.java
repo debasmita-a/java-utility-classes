95b54005e04086b002bef3b75e1ac205221a61b3
Debasmita Adhikari <72942875+debasmita-a@users.noreply.github.com>
2025-03-28 18:52:51 +0530

    🚀 JsonUtil

--- src/main/java/utils/JSONFileReader.java
+++ src/main/java/utils/JSONFileReader.java
@@ -1,5 +1,24 @@
 package utils;
 
-public class JSONFileReader {
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public final class JSONFileReader {
+
+	private JSONFileReader() {
+	}
+
+	static String jsonFilePath = null;
+
+	public static HashMap<String, Object> getData() throws IOException {
+		HashMap<String, Object> map = new ObjectMapper().readValue(new File(jsonFilePath),
+				new TypeReference<HashMap<String, Object>>() {
+				});
+
+		return map;
+	}
 }
